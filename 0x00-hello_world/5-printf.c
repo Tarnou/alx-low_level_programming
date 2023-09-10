edf31d2c1470bc14c03213dca67aeb124d314029
Tarnou <abrahimtarnouhbah@gmail.com>
2023-09-09 19:08:26 -0700

    sixth

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is our function
+ *
+ * Return: our return value is (0)
+ */
+
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

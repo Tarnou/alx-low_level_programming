7a57d0f206dcd81316ee1a0207461c575b771e18
Tarnou <abrahimtarnouhbah@gmail.com>
2023-09-26 07:11:29 -0700

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,12 +3,14 @@
  * print_chessboard - Print a chessboard.
  * @chessboard: The 2D array representing the chessboard.
  */
-void print_chessboard(char chessboard[8][8]) 
+void print_chessboard(char chessboard[8][8])
 {
 	int i, j;
 
-	for (i = 0; i < 8; i++) {
-		for (j = 0; j < 8; j++) {
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
 			_putchar(chessboard[i][j]);
 			_putchar(' ');
 		}
